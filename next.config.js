270ca0451a7e23dd2716f429dc055645acab9eaa
lesser-neoguri <kms345372266@gmail.com>
2025-03-17 19:33:33 +0900

    빌드_시_ESLint_및_TypeScript_검사_비활화

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 필요한 설정이 있을 경우 여기에 추가
+  eslint: {
+    // 빌드 시 ESLint 검사를 비활성화
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 빌드 시 TypeScript 타입 검사를 비활성화
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig; 
